be97f76dbe740e27d60bfc4b78e35e0a7e3e6128
Hyunji Yoon <hyunjiyoon@MacBook-Pro.local>
2021-06-19 19:57:52 -0400

    Add How to Play button and hover state

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
 
-class MyGame extends Phaser.Scene
+class StartScreen extends Phaser.Scene
 {
     constructor ()
     {
@@ -10,21 +10,38 @@ class MyGame extends Phaser.Scene
 
     preload ()
     {
-        this.load.image('logo', logoImg);
+        // this.load.image('logo', logoImg);
     }
       
     create ()
     {
-        const logo = this.add.image(400, 150, 'logo');
+        // const logo = this.add.image(400, 150, 'logo');
       
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
+        // this.tweens.add({
+        //     targets: logo,
+        //     y: 450,
+        //     duration: 2000,
+        //     ease: "Power2",
+        //     yoyo: true,
+        //     loop: -1
+        // });
+
+        const instructionsButton = this.add.rectangle(400, 260, 150, 50, 0x6fc4fc);
+            instructionsButton.setInteractive();
+
+            instructionsButton.on('pointerdown', () => {
+                this.scene.stop ('StartScreen')
+                this.scene.start ('')
+            })
+
+
+
+        const instructionsButtonText = this.add.text(350, 250, "How to Play", {fill: '#83a1a7'});
+            instructionsButtonText.setInteractive();
+
+            instructionsButtonText.on('pointerover', () => instructionsButtonText.setStyle({ fill: '#fef01a'}) )
+                .on('pointerout', () => instructionsButtonText.setStyle({ fill: '#83a1a7'}))
+
     }
 }
 
@@ -33,7 +50,7 @@ const config = {
     parent: 'phaser-example',
     width: 800,
     height: 600,
-    scene: MyGame
+    scene: StartScreen
 };
 
 const game = new Phaser.Game(config);
